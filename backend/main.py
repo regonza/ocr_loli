37d98865c1f617d9b202716862760d738a20c1f5
regonza <gonza181092@gmail.com>
2025-03-02 19:02:22 -0300

    feature: NextJS joins the party

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, File, UploadFile
+from backend.services.ocr import OCRProcessor
+import shutil
+import os
+
+app = FastAPI()
+
+ocr_processor = OCRProcessor(lang="spa")
+
+@app.post("/ocr/")
+async def process_ocr(file: UploadFile = File(...)):
+    """
+    Recibe una imagen, la guarda temporalmente y devuelve el texto extraído.
+    """
+    temp_path = f"temp_{file.filename}"
+    
+    with open(temp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    texto = ocr_processor.extract_text(temp_path)
+
+    os.remove(temp_path)  # Eliminar el archivo temporal
+
+    return {"text": texto}
